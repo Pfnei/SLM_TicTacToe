b2e21aac4725e5818e684172b7866027717d5395
MeMySelfandPfnei <christian.pfneisl@gmail.com>
2025-12-12 14:41:55 +0100

    feature-1: Board: added logic the some methods

--- src/main/java/at.rechnikum/slm/tictactoe/Board.java
+++ src/main/java/at.rechnikum/slm/tictactoe/Board.java
@@ -7,23 +7,39 @@ public class Board {
 	
 	
 	public Board() {
-	
+		this.cells new char[3][3];
+		
 	}
 	
 	public boolean isCellEmpty(int x, int y) {
+		if (this.cells[x][y] = null) return true;
+		return false;
 	}
 	
 	public void placeEmpty(int x, int y, char marker) {
+		this.cells[x][y] = marker;
 	}
 	
 	public boolean isFull() {
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				if (isCellEmpty(x, y)) return true;
+			}
+		}
+		return false;
 	}
-
+	
 	public void clear() {
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				this.cells[x][y] = null;
+			}
+		}
+	
 	}
-
+	
 	public void print() {
 	}
 	
-
+	
 }
