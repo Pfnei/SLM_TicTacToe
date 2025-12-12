e4d30066a180a800959b65f00702e1659ae45029
MeMySelfandPfnei <christian.pfneisl@gmail.com>
2025-12-12 15:53:45 +0100

    bug-108: Board: fxied char comparison for 'null' values

--- src/main/java/at.technikum/slm/tictactoe/Board.java
+++ src/main/java/at.technikum/slm/tictactoe/Board.java
@@ -7,16 +7,19 @@ public class Board {
 	
 	
 	public Board() {
-		this.cells new char[3][3];
+		this.cells = new char[3][3];
 		
 	}
 	
 	public boolean isCellEmpty(int x, int y) {
-		if (this.cells[x][y] = null) return true;
+		if (this.cells[x][y] == '\u0000') return true;
 		return false;
 	}
 	
 	public void place(int x, int y, char marker) {
+		if (marker == '\u0000'){
+			System.out.println("Value '\u0000' not allowed");
+			return;}
 		this.cells[x][y] = marker;
 	}
 	
@@ -32,7 +35,7 @@ public class Board {
 	public void clear() {
 		for (int x = 0; x < 3; x++) {
 			for (int y = 0; y < 3; y++) {
-				this.cells[x][y] = null;
+				this.cells[x][y] = '\u0000';
 			}
 		}
 	
