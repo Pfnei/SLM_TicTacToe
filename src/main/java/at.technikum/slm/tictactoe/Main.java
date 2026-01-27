899d91d459f6077d32a9b859bfce8edfc472381a
lurchcoding <f.4.7.1991@gmx.at>
2026-01-27 16:59:58 +0100

    task-112: add logic for feature-1

--- src/main/java/at.technikum/slm/tictactoe/Main.java
+++ src/main/java/at.technikum/slm/tictactoe/Main.java
@@ -1,9 +1,27 @@
 package at.technikum.slm.tictactoe;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        
+        Board board = new Board();
+        Player player = new Player('X'); 
+        Scanner sc = new Scanner(System.in);
+
+        int row;
+        int column;
+
+        System.out.print("row (0-2): ");
+        row = sc.nextInt();
+
+        System.out.print("column (0-2): ");
+        column = sc.nextInt();
+
+        if (board.isCellEmpty(row, column)) {
+            board.place(row, column, player.getMarker());
+        } else {
+            System.out.println("Sorry, this field is either empty or row and column are not within 0-2");
+        }
         
     }
 }
