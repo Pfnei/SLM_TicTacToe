e2477be6e4c038aebc843065d9d559cd9cc9d1e0
DanyLaptop <q845999@gmail.com>
2026-01-30 13:26:18 +0100

    feature-2/see-game-state: Improves game play and board presentation
    Refactors the main loop to create a more playable game, switching between players after each valid move.
    
    Improves board representation, now showing coordinates and cell separation.

--- src/main/java/at.technikum/slm/tictactoe/Main.java
+++ src/main/java/at.technikum/slm/tictactoe/Main.java
@@ -1,58 +1,53 @@
 package at.technikum.slm.tictactoe;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
-
-        Board board = new Board();
-        Player player = new Player('X');
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        Board board = game.getBoard();
         Scanner sc = new Scanner(System.in);
 
-        int row = -1;    // y
-        int column = -1; // x
+        while (true) {
+            Player current = game.getCurrentPlayer();
+            board.print();
+            System.out.println("Spieler " + current.getMarker() + " ist am Zug.");
 
-        board.print();
+            int row = -1;
+            int column = -1;
 
-        
-        while (row < 0 || row > 2) {
-            try {
-                System.out.print("Row (y) [0-2]: ");
-                row = sc.nextInt();
-                if (row < 0 || row > 2) {
-                    System.out.println("Fehler: Row muss zwischen 0 und 2 liegen!");
-                }
-            }
-            catch  (InputMismatchException e) {
-                System.out.println("Fehler: Bitte eine ganze Zahl eingeben!");
-                sc.nextLine();
-            }
-            
-        }
-        
-        
 
-        while (column < 0 || column > 2) {
-            try {
-                System.out.print("Column (x) [0-2]: ");
-                column = sc.nextInt();
-                if (column < 0 || column > 2) {
-                    System.out.println("Fehler: Column muss zwischen 0 und 2 liegen!");
-                }
-            }
-            catch  (InputMismatchException e) {
+            while (row < 0 || row > 2) {
+                try {
+                    System.out.print("Row (y) [0-2]: ");
+                    row = sc.nextInt();
+                    if (row < 0 || row > 2) System.out.println("Fehler: Row muss zwischen 0 und 2 liegen!");
+                } catch (InputMismatchException e) {
                     System.out.println("Fehler: Bitte eine ganze Zahl eingeben!");
                     sc.nextLine();
+                }
+            }
+
+
+            while (column < 0 || column > 2) {
+                try {
+                    System.out.print("Column (x) [0-2]: ");
+                    column = sc.nextInt();
+                    if (column < 0 || column > 2) System.out.println("Fehler: Column muss zwischen 0 und 2 liegen!");
+                } catch (InputMismatchException e) {
+                    System.out.println("Fehler: Bitte eine ganze Zahl eingeben!");
+                    sc.nextLine();
+                }
+            }
+
+
+            if (board.isCellEmpty(column, row)) {
+                board.place(column, row, current.getMarker());
+                game.switchCurrentPlayer();
+            } else {
+                System.out.println("Dieses Feld ist nicht leer!");
             }
         }
-
-        
-        if (board.isCellEmpty(column, row)) {
-            board.place(column, row, player.getMarker());
-        } else {
-            System.out.println("Dieses Feld ist nicht leer!");
-        }
-
-        board.print();
     }
 }
--- src/main/java/at.technikum/slm/tictactoe/TicTacToe.java
+++ src/main/java/at.technikum/slm/tictactoe/TicTacToe.java
@@ -3,29 +3,50 @@ package at.technikum.slm.tictactoe;
 import java.util.Scanner;
 
 public class TicTacToe {
-	
+
 	private Player player1;
 	private Player player2;
+
+	public Player getCurrentPlayer() {
+		return currentPlayer;
+	}
+
 	private Player currentPlayer;
 	private Board board;
-	
-	
+
+
 	public TicTacToe() {
 		this.board = new Board();
 		this.player1= new Player('X');
 		this.player2= new Player('O');
-	
+		this.currentPlayer = player1;
 	}
-	
+
 	public void start() {
 	}
-	
+
 	public void switchCurrentPlayer() {
+		if(player1 == null || player2 == null){
+			throw new NullPointerException("hmm");
+		}
+		if(currentPlayer.equals(player1)){
+			currentPlayer = player2;
+		}else{
+			currentPlayer = player1;
+		}
+
 	}
-	
+
 	public boolean hasWinner() {
 		return true;
 	}
-	
-	
+
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public Player getPlayer() {
+		return currentPlayer;
+	}
 }
