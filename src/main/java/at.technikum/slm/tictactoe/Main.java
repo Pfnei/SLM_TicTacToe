f7022c747346f990f55bf49554c3fb03770bb5d2
dany <q845999@gmail.com>
2026-01-31 20:51:55 +0100

    feature-2/see-game-state Cleans up unnecessary imports
    Removes unused imports to improve code clarity
    and reduce potential compile-time dependencies.

--- src/main/java/at.technikum/slm/tictactoe/Main.java
+++ src/main/java/at.technikum/slm/tictactoe/Main.java
@@ -1,8 +1,5 @@
 package at.technikum.slm.tictactoe;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         TicTacToe game = new TicTacToe();
