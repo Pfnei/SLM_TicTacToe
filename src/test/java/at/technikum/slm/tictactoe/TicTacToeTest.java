188b8a2bb2f08dc8b8e460a0d28fb30f868a435d
lurchcoding <f.4.7.1991@gmx.at>
2026-02-01 13:50:20 +0100

    task-116: add unit-tests tictactoe.switchcurrentplayer

--- src/test/java/at/technikum/slm/tictactoe/TicTacToeTest.java
+++ src/test/java/at/technikum/slm/tictactoe/TicTacToeTest.java
@@ -11,9 +11,23 @@ class TicTacToeTest {
 	}
 	
 	@Test
-	void switchCurrentPlayer() {
+	void switchCurrentPlayer_switchBackToPlayer1() {
+		TicTacToe game = new TicTacToe();
+		Player player1 = game.getCurrentPlayer();
+		game.switchCurrentPlayer();
+		game.switchCurrentPlayer();
+		assertEquals(player1, game.getCurrentPlayer());
 	}
-	
+
+	@Test
+	void switchCurrentPlayer_switchPlayer1ToPlayer2() {
+		TicTacToe game = new TicTacToe();
+		Player player1 = game.getCurrentPlayer();
+		game.switchCurrentPlayer();
+		Player player2 = game.getCurrentPlayer();
+		assertEquals(player1, player2);
+	}
+
 	@Test
 	void hasWinner() {
 	}
